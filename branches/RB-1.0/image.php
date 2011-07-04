6a938f5c4eb7deec5bd408b58f39ab87d9e32b05
fwpost@gmail.com <fwpost@gmail.com@10f31350-ffcc-fd3b-3c78-7cefcf398c7b>
2011-07-04 18:50:21 +0000

    Creating release branche 1.0

---
+++ branches/RB-1.0/image.php
@@ -0,0 +1,125 @@
+<?php
+
+include('cd.php');
+$CurrentUser = Authentication::Authenticate();
+
+if(!array_key_exists('model_id', $_GET) || !$_GET['model_id'] || !is_numeric($_GET['model_id'])){
+	header('location:index.php');
+}
+
+$ModelID = (int)$_GET['model_id'];
+
+if(!array_key_exists('set_id', $_GET) || !$_GET['set_id'] || !is_numeric($_GET['set_id'])){
+	header('location:set.php?model_id'.$ModelID);
+}
+
+$SetID = (int)$_GET['set_id'];
+
+/* @var $Model Model */
+/* @var $Set Set */
+$Model = null;
+$Set = null;
+$ImageRows = '';
+$ImageCount = 0;
+
+$WhereClause = sprintf('model_id = %1$d AND set_id = %2$d AND mut_deleted = -1', $ModelID, $SetID);
+$Images = Image::GetImages($WhereClause);
+
+if($Images)
+{
+	/* @var $Image Image */
+	foreach($Images as $Image)
+	{
+		$ImageCount++;
+		if(!$Set) { $Set = $Image->getSet(); }
+		if(!$Model) { $Model = $Set->getModel(); }
+
+		$ImageRows .= sprintf(
+		"\n<tr class=\"Row%12\$d\">".
+    		"<td><a href=\"image_view.php?model_id=%3\$d&amp;set_id=%2\$d&amp;image_id=%1\$d\">%4\$s</a></td>".
+        	"<td class=\"Center\">%5\$s</td>".
+	    	"<td class=\"Center\">%6\$s</td>".
+			"<td class=\"Center\">%9\$d</td>".
+			"<td class=\"Center\">%10\$d</td>".
+        	"<td>%7\$s...</td>".
+			"<td>%8\$s</td>".
+			"<td class=\"Center\"><a href=\"download_zip.php?image_id=%1\$d\"><img src=\"images/button_download.png\" width=\"16\" height=\"16\" alt=\"Download image\" title=\"Download image\" /></a></td>".
+			"<td class=\"Center\"><a href=\"download_image.php?image_id=%1\$d\" title=\"%4\$s\" rel=\"lightbox-gal\"><img src=\"images/button_view.png\" width=\"16\" height=\"16\" alt=\"View image\" title=\"View image\" /></a></td>".
+			"<td class=\"Center\"><a href=\"image_view.php?model_id=%3\$d&amp;set_id=%2\$d&amp;image_id=%1\$d&amp;cmd=%11\$s\" title=\"Delete image\"><img src=\"images/button_delete.png\" width=\"16\" height=\"16\" alt=\"Delete\" /></a></td>".
+        "</tr>",
+		$Image->getID(),
+		$Set->getID(),
+		$Model->getID(),
+		htmlentities($Image->getFileName()),
+		htmlentities($Image->getFileExtension()),
+		Utils::ReadableFilesize($Image->getFileSize()),
+		htmlentities(substr($Image->getFileCheckSum(), 0, 16)),
+		$Image->getDateTaken() > 0 ? date('j F Y', $Image->getDateTaken()) : '&nbsp;',
+		$Image->getImageWidth(),
+		$Image->getImageHeight(),
+		COMMAND_DELETE,
+		$ImageCount % 2 == 0 ? 2 : 1
+		);
+	}
+}
+
+if(!$Set)
+{
+	$Set = Set::GetSets(sprintf('model_id = %1$d AND set_id = %2$d', $ModelID, $SetID));
+	if($Set)
+	{
+		$Set = $Set[0];
+		$Model = $Set->getModel();
+	}
+	else
+	{ header('location:index.php'); }
+}
+
+echo HTMLstuff::HtmlHeader(sprintf('%1$s - Set %2$s - Images', $Model->GetShortName(), $Set->getName()), $CurrentUser);
+
+?>
+
+<h2><?php echo sprintf(
+	'<a href="index.php">Home</a> - <a href="model_view.php?model_id=%1$d">%3$s</a> - <a href="set.php?model_id=%1$d">Sets</a> - <a href="set_view.php?model_id=%1$d&amp;set_id=%2$d">Set %4$s</a> - Images',
+	$ModelID,
+	$SetID,
+	htmlentities($Model->GetShortName()),
+	htmlentities($Set->getName())
+); ?></h2>
+
+<table border="0" cellpadding="4" cellspacing="0">
+	<thead>
+		<tr>
+			<th>Filename</th>
+			<th style="width: 60px;">Extension</th>
+			<th style="width: 80px;">Filesize</th>
+			<th style="width: 65px;">Width</th>
+			<th style="width: 65px;">Height</th>
+			<th style="width: 160px;">Checksum</th>
+			<th style="width: 140px;">Date</th>
+			<th style="width: 22px;">&nbsp;</th>
+			<th style="width: 22px;">&nbsp;</th>
+			<th style="width: 22px;">&nbsp;</th>
+		</tr>
+	</thead>
+	<tfoot>
+		<tr>
+			<th colspan="10">Total image count: <?php printf('%1$d', $ImageCount); ?></th>
+		</tr>
+	</tfoot>
+	<tbody>
+	<?php echo $ImageRows ? $ImageRows : '<tr class="Row1"><td colspan="10">&nbsp;</td></tr>'; ?>
+	</tbody>
+</table>
+
+<?php
+echo HTMLstuff::Button(sprintf('image_view.php?model_id=%1$d&amp;set_id=%2$d', $ModelID, $SetID), 'New image');
+
+echo HTMLstuff::Button(sprintf('import_image.php?model_id=%1$d&amp;set_id=%2$d', $ModelID, $SetID), 'Import images');
+
+echo HTMLstuff::Button(sprintf('set.php?model_id=%1$d', $ModelID), 'Sets');
+
+echo HTMLstuff::Button('index.php');
+
+echo HTMLstuff::HtmlFooter();
+?>
---
+++ branches/RB-1.0/password.php
@@ -0,0 +1,207 @@
+<?php
+
+include('cd.php');
+$UserName = null;
+$Hash = null;
+$HashError = false;
+$MailSent = null;
+
+
+if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] && $_POST['hidAction'] == 'PasswordPassword')
+{
+	$UserName = $_POST['txtUserName'];
+
+	$WhereClause = sprintf("user_username = '%1\$s' AND mut_deleted = -1",
+		mysql_real_escape_string($UserName)
+	);
+
+	$Users = User::GetUsers($WhereClause);
+	if($Users)
+	{
+		/* @var $User User */
+		$User = $Users[0];
+
+		$ReturnURL = sprintf("http://%1\$s%2\$s?Hash=%3\$s",
+			$_SERVER['HTTP_HOST'],
+			$_SERVER['PHP_SELF'],
+			$User->getPassword()
+		);
+
+		$ml->AddAddress($User->getEmailAddress(), $User->GetFullName());
+		$ml->Subject = 'Reset your CandyDoll DB password';
+		$ml->Body = sprintf($MailTemplateResetPassword, $User->GetFullName(), $ReturnURL);
+
+		$MailSent = $ml->Send();
+
+		if(!$MailSent)
+		{
+			$MailError = new Error();
+			$MailError->setErrorMessage($ml->ErrorInfo);
+			Error::AddError($MailError);
+		}
+	}
+	else
+	{
+		$LoginError = new LoginError();
+		$LoginError->setErrorNumber(LOGIN_ERR_USERNAMENOTFOUND);
+		$LoginError->setErrorMessage(LoginError::TranslateLoginError(LOGIN_ERR_USERNAMENOTFOUND));
+		Error::AddError($LoginError);
+	}
+}
+else if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] && $_POST['hidAction'] == 'PasswordReset' && array_key_exists('Hash', $_GET) && preg_match('/^[0-9a-f]{40}$/i', $_GET['Hash']))
+{
+	$Hash = $_GET['Hash'];
+
+	$WhereClause = sprintf("user_password = '%1\$s' AND mut_deleted = -1",
+		mysql_real_escape_string($Hash)
+	);
+
+	$Users = User::GetUsers($WhereClause);
+	if($Users)
+	{
+		/* @var $User User */
+		$User = $Users[0];
+
+		if($_POST['txtNewPassword'] && $_POST['txtRepeatPassword'] && $_POST['txtNewPassword'] == $_POST['txtRepeatPassword'])
+		{
+			$User->setSalt(Utils::GenerateGarbage(20));
+			$User->setPassword(Utils::HashString($_POST['txtNewPassword'], $User->getSalt()));
+
+			$User->setPreLastLogin($User->getLastLogin());
+			$User->setLastLogin(time());
+			User::UpdateUser($User, $User);
+				
+			$_SESSION['CurrentUser'] = serialize($User);
+			header('location:index.php');
+		}
+		else
+		{
+			$LoginError = new LoginError();
+			$LoginError->setErrorNumber(LOGIN_ERR_PASSWORDSNOTIDENTICAL);
+			$LoginError->setErrorMessage(LoginError::TranslateLoginError(LOGIN_ERR_PASSWORDSNOTIDENTICAL));
+			Error::AddError($LoginError);
+		}
+	}
+	else
+	{
+		header('location:login.php');
+	}
+}
+else if (!array_key_exists('hidAction', $_POST) && array_key_exists('Hash', $_GET) && preg_match('/^[0-9a-f]{40}$/i', $_GET['Hash']))
+{
+	$Hash = $_GET['Hash'];
+
+	$WhereClause = sprintf("user_password = '%1\$s' AND mut_deleted = -1",
+		mysql_real_escape_string($Hash)
+	);
+
+	$Users = User::GetUsers($WhereClause);
+	if($Users)
+	{
+		/* @var $User User */
+		$User = $Users[0];
+	}
+	else
+	{
+		$LoginError = new LoginError();
+		$LoginError->setErrorNumber(LOGIN_ERR_RESETCODENOTFOUND);
+		$LoginError->setErrorMessage(LoginError::TranslateLoginError(LOGIN_ERR_RESETCODENOTFOUND));
+		Error::AddError($LoginError);
+		$HashError = true;
+	}
+}
+
+echo HTMLstuff::HtmlHeader('Reset your password'); ?>
+
+<div class="CenterForm">
+
+<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+<fieldset><legend>Please log in:</legend>
+
+<?php if(!$Hash && is_null($MailSent)) { ?>
+<input type="hidden" id="hidAction" name="hidAction" value="PasswordPassword" />
+
+<p>Please provide the username of the account for which you would like to reset the password. A hyperlink will then be sent to the account's email address which will enable you to reset the password.</p>
+
+<div class="FormRow">
+<label for="txtUserName">Username: <em>*</em></label>
+<input type="text" id="txtUserName" name="txtUserName" maxlength="50" value="<?php echo $UserName; ?>" />
+</div>
+
+<div class="FormRow">
+<label>&nbsp;</label>
+<input type="submit" id="btnSend" name="btnSend" value="Send" />
+<input type="button" id="btnCancel" name="btnCancel" value="Cancel" onclick="window.location='login.php';" />
+</div>
+
+<?php } ?>
+
+<?php if($Hash && !$HashError){ ?>
+
+<input type="hidden" id="hidAction" name="hidAction" value="PasswordReset" />
+
+<p>Please provide a new password for your account, and repeat it to avoid typing mistakes. Once your password is reset, you will be loggin in automatically.</p>
+
+<div class="FormRow">
+<label>Username:</label>
+<span><?php echo htmlentities($User->getUserName()); ?></span>
+</div>
+
+<div class="FormRow">
+<label for="txtNewPassword">New password: <em>*</em></label>
+<input type="password" id="txtNewPassword" name="txtNewPassword" maxlength="50" />
+</div>
+
+<div class="FormRow">
+<label for="txtRepeatPassword">Repeat password: <em>*</em></label>
+<input type="password" id="txtRepeatPassword" name="txtRepeatPassword" maxlength="50" />
+</div>
+
+<div class="FormRow">
+<label>&nbsp;</label>
+<input type="submit" id="btnSend" name="btnSend" value="Reset" />
+<input type="button" id="btnCancel" name="btnCancel" value="Cancel" onclick="window.location='login.php';" />
+</div>
+
+<?php } ?>
+
+<?php if($HashError){ ?>
+
+<p>The hypelink you have used is not or no longer valid.<br />
+Please return to the login-page.</p>
+
+<div class="FormRow">
+<label>&nbsp;</label>
+<input type="button" id="btnCancel" name="btnCancel" value="Return" onclick="window.location='login.php';" />
+</div>
+
+<?php } ?>
+
+<?php if(!is_null($MailSent) && $MailSent === true){ ?>
+
+<p>An e-mail containing a hyperlink has been sent to your e-mailaddress. Use it ito reset your account's password.</p>
+
+<div class="FormRow">
+<label>&nbsp;</label>
+<input type="button" id="btnCancel" name="btnCancel" value="Return" onclick="window.location='login.php';" />
+</div>
+
+<?php } ?>
+
+<?php if(!is_null($MailSent) && $MailSent === false){ ?>
+
+<p>An error occurred while sending your e-mail. Please contact the system's administrator.</p>
+
+<div class="FormRow">
+<label>&nbsp;</label>
+<input type="button" id="btnCancel" name="btnCancel" value="Return" onclick="window.location='login.php';" />
+</div>
+
+<?php } ?>
+
+</fieldset>
+</form>
+
+</div>
+
+<?php echo HTMLstuff::HtmlFooter(); ?>
