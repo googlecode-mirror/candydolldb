bead1f242d78a2b3482f37459aa2d4d40e4fefe4
fwpost <fwpost@gmail.com>
2011-07-19 19:14:48 +0000

    Fixing a bug, release

--- class/class.global.php
+++ class/class.global.php
@@ -1,4 +1,19 @@
 <?php
+/*	This file is part of CandyDollDB.
+
+    CandyDollDB is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CandyDollDB is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CandyDollDB.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 class Authentication
 {
@@ -33,7 +48,7 @@ class Authentication
 		}
 		else
 		{
-			/* Authenticate on the commandline as Default User */
+			/* Authenticate on the commandline as FWieP */
 			global $argv, $argc;
 			if($argv && $argc > 0)
 			{
