f540647768483467634d359b208f0db93a82514f
fwpost@gmail.com <fwpost@gmail.com@10f31350-ffcc-fd3b-3c78-7cefcf398c7b>
2011-09-24 08:22:47 +0000

    App-wide integration of index generator complete, resize on QS-parameters. Performance is pretty poor, does anyone have a good caching mechanism up her/his sleeve?

--- trunk/model_view.php
+++ trunk/model_view.php
@@ -107,7 +107,7 @@ if($ModelID)
 
 <?php echo $ModelID ? HTMLstuff::Button(sprintf('set.php?model_id=%1$d', $ModelID), 'Sets') : ''; ?>
 
-<?php echo $ModelID ? HTMLstuff::Button(sprintf('download_image.php?model_id=%1$d', $ModelID), 'Thumbnails', 'rel="lightbox"') : ''; ?>
+<?php echo $ModelID ? HTMLstuff::Button(sprintf('download_index.php?model_id=%1$d&amp;width=500&amp;height=750', $ModelID), 'Thumbnails', 'rel="lightbox"') : ''; ?>
 
 <?php echo HTMLstuff::Button('index.php'); ?>
 
