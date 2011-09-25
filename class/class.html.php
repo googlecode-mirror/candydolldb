f105746f99a2918264a902fcd0b5f72c9c868a1b
fwpost <fwpost@gmail.com>
2011-09-25 09:57:39 +0000

    Creating tag branch 1.3

--- class/class.html.php
+++ class/class.html.php
@@ -1,4 +1,19 @@
 <?php
+/*	This file is part of CandyDollDB.
+
+    CandyDollDB is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CandyDollDB is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CandyDollDB.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 class HTMLstuff
 {
@@ -24,7 +39,7 @@ class HTMLstuff
 		<script type=\"text/javascript\" src=\"http://code.jquery.com/jquery.min.js\"></script>
 		<script type=\"text/javascript\" src=\"js/fwiep.js\"></script>
 		%5\$s
-		<title>CandyDoll DB%1\$s</title>
+		<title>CandyDoll DB v1.3%1\$s</title>
 		</head>
 
 		<body>
@@ -32,7 +47,7 @@ class HTMLstuff
 		<div id=\"Container\">
 		<div id=\"Header\"%3\$s>
 
-		<h1>CandyDoll DB%1\$s</h1>
+		<h1>CandyDoll DB v1.3%1\$s</h1>
 		<p>by <a href=\"http://www.fwiep.nl/\" rel=\"external\" title=\"FWieP\">FWieP</a></p>
 		%4\$s
 		<div class=\"AbsolutePoint\">
--- js/fwiep.js
+++ js/fwiep.js
@@ -1,3 +1,18 @@
+/*	This file is part of CandyDollDB.
+
+    CandyDollDB is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CandyDollDB is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CandyDollDB.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /*
 	Slimbox v2.04 - The ultimate lightweight Lightbox clone for jQuery
 	(c) 2007-2010 Christophe Beyls <http://www.digitalia.be>
