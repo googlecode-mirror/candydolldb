e7101383ee5a16eec9366e7ff7ce5d5ccb168d29
fwpost@gmail.com <fwpost@gmail.com@10f31350-ffcc-fd3b-3c78-7cefcf398c7b>
2012-01-28 18:13:32 +0000

    Creating release branch 1.5

---
+++ branches/RB-1.5/cacheimage_nuke.php
@@ -0,0 +1,39 @@
+<?php
+
+include('cd.php');
+$CurrentUser = Authentication::Authenticate();
+
+
+$CacheFolder = null;
+$CacheImages = CacheImage::GetCacheImages();
+
+
+if(isset($argv) && $argc > 0)
+{ $CacheFolder = sprintf('%1$s/cache', dirname($_SERVER['PHP_SELF'])); }
+else
+{ $CacheFolder = 'cache'; }
+
+
+/* @var $it RecursiveIteratorIterator */
+$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(
+	$CacheFolder,
+	FileSystemIterator::SKIP_DOTS | FileSystemIterator::CURRENT_AS_FILEINFO
+));
+
+/* @var $file SplFileInfo */
+foreach($it as $file)
+{
+	$idToFind = $file->getBasename('.jpg');
+	
+	if(!preg_match('/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i', $idToFind))
+	{ continue; }
+	
+	$CacheImageInDB = CacheImage::FilterCacheImages($CacheImages, null, null, null, null, null, null, $idToFind);
+	
+	if(!$CacheImageInDB)
+	{ unlink($file->getRealPath()); }	
+}
+
+HTMLstuff::RefererRedirect();
+
+?>
