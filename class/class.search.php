8789abd0203c050aa1598fd042ce576f7afff051
fwpost <fwpost@gmail.com>
2012-03-28 20:23:45 +0000

    New tagsearching. No GUI yet, but it works!

--- class/class.search.php
+++ class/class.search.php
@@ -1,78 +1,8 @@
 <?php
 
-class AllIDs
-{
-	private $ModelID;
-	private $SetID;
-	private $ImageID;
-	private $VideoID;
-	
-	public function getModelID()
-	{ return $this->ModelID; }
-	
-	public function setModelID($ModelID)
-	{ $this->ModelID = $ModelID; }
-	
-	public function getSetID()
-	{ return $this->SetID; }
-	
-	public function setSetID($SetID)
-	{ $this->SetID = $SetID; }
-	
-	public function getImageID()
-	{ return $this->ImageID; }
-	
-	public function setImageID($ImageID)
-	{ $this->ImageID = $ImageID; }
-	
-	public function getVideoID()
-	{ return $this->VideoID; }
-	
-	public function setVideoID($VideoID)
-	{ $this->VideoID = $VideoID; }
-	
-	public static function GetAllIDs($WhereClause = null, $OrderClause = null, $LimitClause = null)
-	{
-		global $db;
-	
-		if($db->Select('vw_IDs', '*', $WhereClause, $OrderClause, $LimitClause))
-		{
-			$OutArray = array();
-			if($db->getResult())
-			{
-				foreach($db->getResult() as $IDItem)
-				{
-					$OutArray[] = self::ProcessDBitem($IDItem);
-				}
-			}
-			return $OutArray;
-		}
-		else
-		{ return null; }
-	}
-	
-	public static function ProcessDBitem($IDItem)
-	{
-		$IDObject = new AllIDs();
-			
-		foreach($IDItem as $ColumnKey => $ColumnValue)
-		{
-			switch($ColumnKey)
-			{
-				case 'model_id'	: $IDObject->setModelID($ColumnValue);	break;
-				case 'set_id'	: $IDObject->setSetID($ColumnValue);	break;
-				case 'image_id'	: $IDObject->setImageID($ColumnValue);	break;
-				case 'video_id'	: $IDObject->setVideoID($ColumnValue);	break;
-			}
-		}
-	
-		return $IDObject;
-	}
-}
-
 class Search
 {
-
+	
 }
 
 ?>
--- search.php
+++ search.php
@@ -4,25 +4,111 @@ include('cd.php');
 $CurrentUser = Authentication::Authenticate();
 
 $q = null;
-$Models = array();
 $Tags = Tag::GetTags();
-
-// Unfortunately, this is a killer-query. It maxes out my 128M PHP-memory limit... :(
-//$AllIDs = AllIDs::GetAllIDs();
-
-$Tag2Alls = Tag2All::GetTag2Alls();
+$Tag2Alls = array();
 
 
 if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 {
+	/* Core search-processing */
 	$q = $_POST['q'];
-	
 	$filteredTags = Tag::FilterTagsByCSV($Tags, $q);
 	$filteredTagIDs = array();
 	
 	foreach($filteredTags as $t){
 		$filteredTagIDs[] = $t->getID();
 	}
+	
+	/* Fetch Tag2Alls if tags were entered, default to no results when $q is empty. */
+	$whereClause = $filteredTagIDs ? sprintf('tag_id IN ( %1$s )', join(', ', $filteredTagIDs)) : '1 = 0';
+	$Tag2Alls = Tag2All::GetTag2Alls($whereClause);
+	
+	
+	
+	/* Model-filtering */
+	$AllModelIDs = array();
+	foreach($Tag2Alls as $t2a){
+		$AllModelIDs[] = $t2a->getModelID();
+	}
+	
+	$ModelIDsToShow = array();
+	foreach(array_unique($AllModelIDs) as $modelid)
+	{
+		foreach ($filteredTagIDs as $ftid)
+		{
+			if(count(Tag2All::FilterTag2Alls($Tag2Alls, $ftid, $modelid, null, null, null)) == 0)
+			{ continue 2; }
+		}
+		$ModelIDsToShow[] = $modelid;
+	}
+	
+	
+	/* Set-filtering */
+	$AllSetIDs = array();
+	foreach($Tag2Alls as $t2a){
+		$AllSetIDs[] = $t2a->getSetID();
+	}
+	
+	$SetIDsToShow = array();
+	foreach(array_unique($AllSetIDs) as $setid)
+	{
+		foreach ($filteredTagIDs as $ftid)
+		{
+			if(count(Tag2All::FilterTag2Alls($Tag2Alls, $ftid, null, $setid, null, null)) == 0)
+			{ continue 2; }
+		}
+		$SetIDsToShow[] = $setid;
+	}
+
+	
+	/* Image-filtering */
+	$AllImageIDs = array();
+	foreach($Tag2Alls as $t2a){
+		$AllImageIDs[] = $t2a->getImageID();
+	}
+	
+	$ImageIDsToShow = array();
+	foreach(array_unique($AllImageIDs) as $imageid)
+	{
+		foreach ($filteredTagIDs as $ftid)
+		{
+			if(count(Tag2All::FilterTag2Alls($Tag2Alls, $ftid, null, null, $imageid, null)) == 0)
+			{ continue 2; }
+		}
+		$ImageIDsToShow[] = $imageid;
+	}
+	
+	
+	/* Video-filtering */
+	$AllVideoIDs = array();
+	foreach($Tag2Alls as $t2a){
+		$AllVideoIDs[] = $t2a->getVideoID();
+	}
+	
+	$VideoIDsToShow = array();
+	foreach(array_unique($AllVideoIDs) as $videoid)
+	{
+		foreach ($filteredTagIDs as $ftid)
+		{
+			if(count(Tag2All::FilterTag2Alls($Tag2Alls, $ftid, null, null, null, $videoid)) == 0)
+			{ continue 2; }
+		}
+		$VideoIDsToShow[] = $videoid;
+	}
+	
+	
+	
+	/* @TODO
+	 * Fetch and show what we want to see:
+	 * 
+	 * Model::Fetch(model_id in $ModelIDsToShow)	or
+	 * Set::Fetch(set_id in $SetIDsToShow)			or
+	 * Image::Fetch(image_id in $ImageIDsToShow)	or
+	 * Video::Fetch(video_id in $VideoIDsToShow)
+	 * 
+	 * Construct DIVs with thumbnails, etc.
+	 */
+	
 }
 
 echo HTMLstuff::HtmlHeader('Tag search', $CurrentUser);
@@ -38,10 +124,6 @@ echo HTMLstuff::HtmlHeader('Tag search', $CurrentUser);
 
 <input type="text" id="q" name="q" class="TagsBox" value="<?php echo $q; ?>" />
 <input type="submit" class="FormButton" value="Search" />
-
-<div class="Separator"></div>
-
-<?php var_dump($Models); ?>
 
 <div class="Separator"></div>
 
