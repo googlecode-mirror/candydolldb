c1d5da1313f3c7c65b7838a8f85f7ed1d4a42e5e
mranimosityfish@gmail.com <mranimosityfish@gmail.com@10f31350-ffcc-fd3b-3c78-7cefcf398c7b>
2012-03-30 16:37:16 +0000

    TagSearch GUI, work in progress

--- trunk/search.php
+++ trunk/search.php
@@ -7,12 +7,15 @@ $q = null;
 $searchMode = 'MODEL';
 $Tags = Tag::GetTags();
 $Tag2Alls = array();
+$ModelCount = 0;
+$Modelprint = '';
 
 
 if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 {
 	/* Core search-processing */
 	$q = $_POST['q'];
+	$searchMode  = $_POST['selectType'];
 	$filteredTags = Tag::FilterTagsByCSV($Tags, $q);
 	$filteredTagIDs = array();
 	
@@ -160,7 +163,67 @@ if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 	
 	echo "VideoIDs => ";
 	var_dump($VideoIDsToShow);
-	
+	if($ToShow)
+	{
+		foreach($ToShow as $Model)
+		{
+		$ModelCount++;
+		switch ($searchMode)
+			{
+				case "MODEL":
+					$Modelprint .= sprintf(
+			"<div class=\"ThumbGalItem\">
+			<h3 class=\"Hidden\">%1\$s</h3>
+			<div class=\"ThumbImageWrapper\">
+			<a href=\"set.php?model_id=%2\$d\">
+			<img src=\"download_image.php?model_id=%2\$d&amp;portrait_only=true&amp;width=150&amp;height=225\" width=\"150\" height=\"225\" alt=\"%1\$s\" title=\"%1\$s\" />
+			</a>
+			</div>
+			<div class=\"ThumbDataWrapper\">
+			<ul>
+			<li>Name: %1\$s</li>
+			</ul>
+			</div>
+			</div>
+			%3\$s",
+			htmlentities($Model->GetFullName()),
+			$Model->getID(),
+			($ModelCount % 4 == 0 ? "<div class=\"Clear\"></div>" : null)
+			);
+				break;
+				case "SET":
+				$Modelprint .= sprintf(
+				"<div class=\"SetThumbGalItem\">
+				<h3 class=\"Hidden\">%1\$s set %2\$s</h3>
+				<div class=\"SetThumbImageWrapper\">
+				<a href=\"image.php?model_id=%3\$d&amp;set_id=%4\$d\">
+				<img src=\"download_image.php?set_id=%4\$d&amp;landscape_only=true&amp;width=225&amp;height=150\" height=\"150\" alt=\"%1\$s set %2\$s\" title=\"%1\$s set %2\$s\" />
+				</a>
+				</div>
+				<div class=\"SearchThumbDataWrapper\">
+					<ul>
+				<li>Model: %s</li>
+				<li>Name: %s</li>
+				</ul>
+				</div>
+				</div>
+				%5\$s",
+				htmlentities($Model->getModel()->GetFullName()),
+				htmlentities($Model->getName()),
+				$Model->getModel()->getID(),
+				$Model->getID(),
+				($ModelCount % 3 == 0 ? "<div class=\"Clear\"></div>" : null)
+				);
+				break;
+				case "IMAGE":
+				$Modelprint = "No Results";
+				break;
+				case "VIDEO":
+				$Modelprint = "No Results";
+				break;
+			}
+		}
+	}
 }
 
 echo HTMLstuff::HtmlHeader('Tag search', $CurrentUser);
@@ -169,16 +232,30 @@ echo HTMLstuff::HtmlHeader('Tag search', $CurrentUser);
 
 <h2><a href="index.php">Home</a> - Tag search</h2>
 
-<form action="<?php echo htmlentities($_SERVER['REQUEST_URI']);?>" method="post">
+<form action="<?php echo htmlentities($_SERVER['REQUEST_URI']);?>" method="post" class="Search">
 <fieldset><legend>Please fill in these fields:</legend>
 
 <input type="hidden" id="hidAction" name="hidAction" value="Search" />
 
+<label for="selectType">Search For:</label>
+<select id="selectType" name="selectType">
+	<option value="SET" <?php echo $searchMode == "SET" ? ' selected="selected"' : null ?>>Sets</option>
+	<option value="MODEL" <?php echo $searchMode == "MODEL" ? ' selected="selected"' : null ?>>Models</option>
+	<option value="IMAGE" <?php echo $searchMode == "IMAGE" ? ' selected="selected"' : null ?>>Images</option>
+	<option value="VIDEO" <?php echo $searchMode == "VIDEO" ? ' selected="selected"' : null ?>>Videos</option>
+</select>
+<label for="q">Tagged with
+</label>
 <input type="text" id="q" name="q" class="TagsBox" value="<?php echo $q; ?>" />
 <input type="submit" class="FormButton" value="Search" />
 
 <div class="Separator"></div>
+<?php
 
+echo "<div class=\"Clear\"></div>".$Modelprint."<div class=\"Clear\"></div>";
+echo "<div style=\"font-weight: bold;text-align: center\">".$ModelCount." results returned</div>";
+?>
+<div class="Separator"></div>
 <?php echo HTMLstuff::Button('index.php'); ?>
 
 </fieldset>
