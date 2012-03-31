38db2c5cfea50c39c14a7328f88eb315c08cdebb
fwpost <fwpost@gmail.com>
2012-03-31 16:46:07 +0000

    Tweaking of TagSearch-GUI, clean-up

--- search.php
+++ search.php
@@ -3,19 +3,21 @@
 include('cd.php');
 $CurrentUser = Authentication::Authenticate();
 
-$q = null;
-$searchMode = 'MODEL';
 $Tags = Tag::GetTags();
+
+$searchMode = 'SET';
+$q = null;
+$ToShow = array();
 $Tag2Alls = array();
-$ModelCount = 0;
-$Modelprint = '';
+$ItemCount = 0;
+$Results = '';
 
 
 if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 {
 	/* Core search-processing */
 	$q = $_POST['q'];
-	$searchMode  = $_POST['selectType'];
+	$searchMode = $_POST['selectType'];
 	$filteredTags = Tag::FilterTagsByCSV($Tags, $q);
 	$filteredTagIDs = array();
 	
@@ -100,127 +102,94 @@ if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 		$VideoIDsToShow[] = $videoid;
 	}
 	
-	
-	
-	/* @TODO
-	 * Fetch and show what we want to see:
-	 * 
-	 * Model::Fetch(model_id in $ModelIDsToShow)
-	 * Set::Fetch(set_id in $SetIDsToShow or model_id in $ModelIDsToShow)
-	 * Image::Fetch(image_id in $ImageIDsToShow or set_id in $SetIDsToShow)
-	 * Video::Fetch(video_id in $VideoIDsToShow or set_id in $SetIDsToShow)
-	 * 
-	 * Construct DIVs with thumbnails, etc.
-	 */
-	
 	switch ($searchMode)
 	{
-		
 		case 'MODEL':
-			if(!$ModelIDsToShow){
-				break;
-			}
+			if(!$ModelIDsToShow){ break; }
 			$where = sprintf('model_id in ( %1$s ) AND mut_deleted = -1', join(', ', $ModelIDsToShow));
 			$ToShow = Model::GetModels($where);
 			break;
-
 			
 		case 'SET':
-			if(!$SetIDsToShow && !$ModelIDsToShow){
-				break;
-			}
-			$where = sprintf('set_id in ( %1$s ) AND mut_deleted = -1',
-				join(', ', $SetIDsToShow)
-			);
+			if(!$SetIDsToShow || !$ModelIDsToShow){ break; }
+			$where = sprintf('set_id in ( %1$s ) AND mut_deleted = -1',	join(', ', $SetIDsToShow));
 			$ToShow = Set::GetSets($where);
 			break;
-			
 			
 		case 'IMAGE':
 			break;
 			
-			
 		case 'VIDEO':
 			break;
 	}
-	
-	
-	echo "ModelIDs => ";
-	var_dump($ModelIDsToShow);
-	
-	echo "<br />\n";
-	
-	echo "SetIDs => ";
-	var_dump($SetIDsToShow);
-	
-	echo "<br />\n";
-	
-	echo "ImageIDs => ";
-	var_dump($ImageIDsToShow);
-	
-	echo "<br />\n";
-	
-	echo "VideoIDs => ";
-	var_dump($VideoIDsToShow);
+
 	if($ToShow)
 	{
-		foreach($ToShow as $Model)
-		{
-		$ModelCount++;
 		switch ($searchMode)
-			{
-				case "MODEL":
-					$Modelprint .= sprintf(
-			"<div class=\"ThumbGalItem\">
-			<h3 class=\"Hidden\">%1\$s</h3>
-			<div class=\"ThumbImageWrapper\">
-			<a href=\"set.php?model_id=%2\$d\">
-			<img src=\"download_image.php?model_id=%2\$d&amp;portrait_only=true&amp;width=150&amp;height=225\" width=\"150\" height=\"225\" alt=\"%1\$s\" title=\"%1\$s\" />
-			</a>
-			</div>
-			<div class=\"ThumbDataWrapper\">
-			<ul>
-			<li>Name: %1\$s</li>
-			</ul>
-			</div>
-			</div>
-			%3\$s",
-			htmlentities($Model->GetFullName()),
-			$Model->getID(),
-			($ModelCount % 4 == 0 ? "<div class=\"Clear\"></div>" : null)
-			);
+		{
+			case "MODEL":
+				foreach($ToShow as $Model)
+				{
+					/* @var $Model Model */
+					$ItemCount++;
+					$Results .= sprintf(
+						"<div class=\"ThumbGalItem\">
+							<h3 class=\"Hidden\">%1\$s</h3>
+							<div class=\"ThumbImageWrapper\">
+								<a href=\"set.php?model_id=%2\$d\" title=\"%1\$s\">
+									<img src=\"download_image.php?model_id=%2\$d&amp;portrait_only=true&amp;width=150&amp;height=225\" width=\"150\" height=\"225\" alt=\"%1\$s\" />
+								</a>
+							</div>
+							<div class=\"SearchThumbDataWrapper\">
+								<ul>
+									<li>Name: %1\$s</li>
+								</ul>
+							</div>
+						</div>
+						%3\$s",
+						htmlentities($Model->GetFullName()),
+						$Model->getID(),
+						($ItemCount % 4 == 0 ? "<div class=\"Clear\"></div>" : null)
+					);
+				}
 				break;
-				case "SET":
-				$Modelprint .= sprintf(
-				"<div class=\"SetThumbGalItem\">
-				<h3 class=\"Hidden\">%1\$s set %2\$s</h3>
-				<div class=\"SetThumbImageWrapper\">
-				<a href=\"image.php?model_id=%3\$d&amp;set_id=%4\$d\">
-				<img src=\"download_image.php?set_id=%4\$d&amp;landscape_only=true&amp;width=225&amp;height=150\" height=\"150\" alt=\"%1\$s set %2\$s\" title=\"%1\$s set %2\$s\" />
-				</a>
-				</div>
-				<div class=\"SearchThumbDataWrapper\">
-					<ul>
-				<li>Model: %s</li>
-				<li>Name: %s</li>
-				</ul>
-				</div>
-				</div>
-				%5\$s",
-				htmlentities($Model->getModel()->GetFullName()),
-				htmlentities($Model->getName()),
-				$Model->getModel()->getID(),
-				$Model->getID(),
-				($ModelCount % 3 == 0 ? "<div class=\"Clear\"></div>" : null)
-				);
+				
+			case "SET":
+				foreach($ToShow as $Set)
+				{
+					/* @var $Set Set */
+					$ItemCount++;
+					$Results .= sprintf(
+						"<div class=\"SetThumbGalItem\">
+							<h3 class=\"Hidden\">%1\$s set %3\$s</h3>
+							<div class=\"SetThumbImageWrapper\">
+								<a href=\"image.php?model_id=%4\$d&amp;set_id=%5\$d\" title=\"%1\$s set %3\$s\">
+									<img src=\"download_image.php?set_id=%5\$d&amp;landscape_only=true&amp;width=225&amp;height=150\" height=\"150\" alt=\"%1\$s set %3\$s\" />
+								</a>
+							</div>
+							<div class=\"SearchThumbDataWrapper\">
+								<ul>
+									<li>Model: %1\$s</li>
+									<li>Setname: %3\$s</li>
+								</ul>
+							</div>
+						</div>
+						%6\$s",
+						htmlentities($Set->getModel()->GetFullName()),
+						htmlentities($Set->getPrefix()),
+						htmlentities($Set->getName()),
+						$Set->getModel()->getID(),
+						$Set->getID(),
+						($ItemCount % 3 == 0 ? "<div class=\"Clear\"></div>" : null)
+					);
+				}
 				break;
-				case "IMAGE":
-				$Modelprint = "No Results";
+					
+			case "IMAGE":
 				break;
-				case "VIDEO":
-				$Modelprint = "No Results";
+				
+			case "VIDEO":
 				break;
-			}
 		}
 	}
 }
@@ -236,25 +205,30 @@ echo HTMLstuff::HtmlHeader('Tag search', $CurrentUser);
 
 <input type="hidden" id="hidAction" name="hidAction" value="Search" />
 
-<label for="selectType">Search For:</label>
+<label for="selectType">Search for</label>
 <select id="selectType" name="selectType">
-	<option value="SET" <?php echo $searchMode == "SET" ? ' selected="selected"' : null ?>>Sets</option>
 	<option value="MODEL" <?php echo $searchMode == "MODEL" ? ' selected="selected"' : null ?>>Models</option>
+	<option value="SET" <?php echo $searchMode == "SET" ? ' selected="selected"' : null ?>>Sets</option>
 	<option value="IMAGE" <?php echo $searchMode == "IMAGE" ? ' selected="selected"' : null ?>>Images</option>
 	<option value="VIDEO" <?php echo $searchMode == "VIDEO" ? ' selected="selected"' : null ?>>Videos</option>
 </select>
-<label for="q">Tagged with
-</label>
-<input type="text" id="q" name="q" class="TagsBox" value="<?php echo $q; ?>" />
+
+<label for="q">tagged with</label>
+<input type="text" id="q" name="q" class="TagsBox" style="width:650px;" value="<?php echo $q; ?>" />
 <input type="submit" class="FormButton" value="Search" />
 
 <div class="Separator"></div>
-<?php
 
-echo "<div class=\"Clear\"></div>".$Modelprint."<div class=\"Clear\"></div>";
-echo "<div style=\"font-weight: bold;text-align: center\">".$ModelCount." results returned</div>";
-?>
+<div class="Clear"></div>
+<?php echo $Results ?>
+<div class="Clear"></div>
+
+<div class="SearchSummary">
+<p><?php echo $ItemCount ?> result(s) returned</p>
+</div>
+
 <div class="Separator"></div>
+
 <?php echo HTMLstuff::Button('index.php'); ?>
 
 </fieldset>
