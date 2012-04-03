c9b9db408e5e3dd1418f12ca619314b3954d5470
mranimosityfish@gmail.com <mranimosityfish@gmail.com@10f31350-ffcc-fd3b-3c78-7cefcf398c7b>
2012-04-03 16:21:47 +0000

    Customizable pagination added to Tagsearch

--- trunk/search.php
+++ trunk/search.php
@@ -5,38 +5,61 @@ $CurrentUser = Authentication::Authenticate();
 
 $Tags = Tag::GetTags();
 
-$searchMode = 'SET';
+$TotalPages = 0;
+$Total = null;
+$searchMode = null;
 $q = null;
 $ToShow = array();
 $Tag2Alls = array();
 $ItemCount = 0;
 $Results = '';
 
+if (isset($_GET['page']))
+{ $page = $_GET['page']; }
+else
+{ $page = 1; }
 
-if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
+if (isset($_GET['txtResults']))
+{ $max_results = $_GET['txtResults']; }
+else
+{ $max_results = 30; }
+
+$from = (($page * $max_results) - $max_results);
+
+if(array_key_exists('hidAction', $_GET) && $_GET['hidAction'] == 'Search')
 {
-	/* Core search-processing */
-	$q = $_POST['q'];
-	$searchMode = $_POST['selectType'];
+	$q = $_SESSION['q']		= $_GET['q'];
+	$searchMode = $_SESSION['selectType'] 		= $_GET['selectType'];
+}
+else
+{
+	$q = array_key_exists('q', $_SESSION) ? $_SESSION['q'] : '';
+	$searchMode = array_key_exists('selectType', $_SESSION) ? $_SESSION['selectType'] : 'SET';
+}
+
+if(array_key_exists('hidAction', $_GET) && $_GET['hidAction'] == 'Search')
+{
+
+/* Core search-processing */
 	$filteredTags = Tag::FilterTagsByCSV($Tags, $q);
 	$filteredTagIDs = array();
-	
+
 	foreach($filteredTags as $t){
 		$filteredTagIDs[] = $t->getID();
 	}
-	
+
 	/* Fetch Tag2Alls if tags were entered, default to no results when $q is empty. */
 	$whereClause = $filteredTagIDs ? sprintf('tag_id IN ( %1$s )', join(', ', $filteredTagIDs)) : '1 = 0';
 	$Tag2Alls = Tag2All::GetTag2Alls($whereClause);
-	
-	
-	
+
+
+
 	/* Model-filtering */
 	$AllModelIDs = array();
 	foreach($Tag2Alls as $t2a){
 		$AllModelIDs[] = $t2a->getModelID();
 	}
-	
+
 	$ModelIDsToShow = array();
 	foreach(array_unique($AllModelIDs) as $modelid)
 	{
@@ -47,14 +70,14 @@ if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 		}
 		$ModelIDsToShow[] = $modelid;
 	}
-	
-	
+
+
 	/* Set-filtering */
 	$AllSetIDs = array();
 	foreach($Tag2Alls as $t2a){
 		$AllSetIDs[] = $t2a->getSetID();
 	}
-	
+
 	$SetIDsToShow = array();
 	foreach(array_unique($AllSetIDs) as $setid)
 	{
@@ -66,13 +89,13 @@ if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 		$SetIDsToShow[] = $setid;
 	}
 
-	
+
 	/* Image-filtering */
 	$AllImageIDs = array();
 	foreach($Tag2Alls as $t2a){
 		$AllImageIDs[] = $t2a->getImageID();
 	}
-	
+
 	$ImageIDsToShow = array();
 	foreach(array_unique($AllImageIDs) as $imageid)
 	{
@@ -83,14 +106,14 @@ if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 		}
 		$ImageIDsToShow[] = $imageid;
 	}
-	
-	
+
+
 	/* Video-filtering */
 	$AllVideoIDs = array();
 	foreach($Tag2Alls as $t2a){
 		$AllVideoIDs[] = $t2a->getVideoID();
 	}
-	
+
 	$VideoIDsToShow = array();
 	foreach(array_unique($AllVideoIDs) as $videoid)
 	{
@@ -101,36 +124,40 @@ if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 		}
 		$VideoIDsToShow[] = $videoid;
 	}
-	
+
 	switch ($searchMode)
 	{
 		case 'MODEL':
 			if(!$ModelIDsToShow){ break; }
 			$where = sprintf('model_id in ( %1$s ) AND mut_deleted = -1', join(', ', $ModelIDsToShow));
-			$ToShow = Model::GetModels($where);
+			$ToShow = Model::GetModels($where, null, sprintf("%1\$d, %2\$d", $from, $max_results));
+			$Total = count(Model::GetModels($where));
 			break;
-			
+
 		case 'SET':
 			if(!$SetIDsToShow || !$ModelIDsToShow){ break; }
 			$where = sprintf('set_id in ( %1$s ) AND mut_deleted = -1',	join(', ', $SetIDsToShow));
-			$ToShow = Set::GetSets($where);
+			$ToShow = Set::GetSets($where, null, sprintf("%1\$d, %2\$d", $from, $max_results));
+			$Total = count(Set::GetSets($where));
 			break;
-			
+
 		case 'IMAGE':
 			if(!$SetIDsToShow || !$ModelIDsToShow){ break; }
 			$where = $ImageIDsToShow ? sprintf('(image_id in ( %1$s ) OR set_id in ( %2$s )) AND mut_deleted = -1',	join(', ', $ImageIDsToShow), join(', ', $SetIDsToShow)) : null;
 			$where = $where ? $where : sprintf('set_id in ( %1$s ) AND mut_deleted = -1',	join(', ', $SetIDsToShow));
-			$ToShow = Image::GetImages($where);
+			$ToShow = Image::GetImages($where, null, sprintf("%1\$d, %2\$d", $from, $max_results));
+			$Total = count(Image::GetImages($where));
 			break;
-			
+
 		case 'VIDEO':
 			if(!$SetIDsToShow || !$ModelIDsToShow){ break; }
 			$where = $VideoIDsToShow ? sprintf('(video_id in ( %1$s ) OR set_id in ( %2$s )) AND mut_deleted = -1',	join(', ', $VideoIDsToShow), join(', ', $SetIDsToShow)) : null;
 			$where = $where ? $where : sprintf('set_id in ( %1$s ) AND mut_deleted = -1',	join(', ', $SetIDsToShow));
-			$ToShow = Video::GetVideos($where);
+			$ToShow = Video::GetVideos($where, null, sprintf("%1\$d, %2\$d", $from, $max_results));
+			$Total = count(Video::GetVideos($where));
 			break;
 	}
-
+	$TotalPages = ceil($Total / $max_results);
 	if($ToShow)
 	{
 		switch ($searchMode)
@@ -161,7 +188,7 @@ if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 					);
 				}
 				break;
-				
+
 			case "SET":
 				foreach($ToShow as $Set)
 				{
@@ -192,7 +219,7 @@ if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 					);
 				}
 				break;
-					
+
 			case "IMAGE":
 				foreach($ToShow as $Image)
 				{
@@ -225,7 +252,7 @@ if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'Search')
 					);
 				}
 				break;
-				
+
 			case "VIDEO":
 				foreach($ToShow as $Video)
 				{
@@ -268,7 +295,7 @@ echo HTMLstuff::HtmlHeader('Tag search', $CurrentUser);
 
 <h2><a href="index.php">Home</a> - Tag search</h2>
 
-<form action="<?php echo htmlentities($_SERVER['REQUEST_URI']);?>" method="post" class="Search">
+<form action="<?php echo htmlentities($_SERVER['REQUEST_URI']);?>" method="get" class="Search">
 <fieldset><legend>Please fill in these fields:</legend>
 
 <input type="hidden" id="hidAction" name="hidAction" value="Search" />
@@ -284,6 +311,10 @@ echo HTMLstuff::HtmlHeader('Tag search', $CurrentUser);
 <label for="q">tagged with</label>
 <input type="text" id="q" name="q" class="TagsBox" style="width:650px;" value="<?php echo $q; ?>" />
 <input type="submit" class="FormButton" value="Search" />
+</br>
+<label for="txtResults">Results per page</label>
+<input type="text" id="txtResults" name="txtResults" class="TagsBox" style="width:50px;" value="<?php echo $max_results; ?>" />
+
 
 <div class="Separator"></div>
 
@@ -292,7 +323,22 @@ echo HTMLstuff::HtmlHeader('Tag search', $CurrentUser);
 <div class="Clear"></div>
 
 <div class="SearchSummary">
-<p><?php echo $ItemCount ?> result(s) returned</p>
+<?php
+if($page > 1)
+{
+echo HTMLStuff::Button('search.php?page=1&hidAction=Search&selectType='.$searchMode.'&q='.$q.'&txtResults='.$max_results, 'First Page', " style=\"float:left\"");
+echo HTMLStuff::Button('search.php?page='.($page-1).'&hidAction=Search&selectType='.$searchMode.'&q='.$q.'&txtResults='.$max_results, 'Previous Page', " style=\"float:left\"");
+}
+
+if($page < $TotalPages)
+{
+echo HTMLStuff::Button('search.php?page='.($TotalPages).'&hidAction=Search&selectType='.$searchMode.'&q='.$q.'&txtResults='.$max_results, 'Last Page', " style=\"float:right\"");
+echo HTMLStuff::Button('search.php?page='.($page+1).'&hidAction=Search&selectType='.$searchMode.'&q='.$q.'&txtResults='.$max_results, 'Next Page', " style=\"float:right\"");
+}
+?>
+<div class="Clear"></div>
+
+<p>Showing <?php echo $Total ? (($from+1).' to '.(($from+$max_results > $Total) ? $Total : $from+$max_results).' of '.$Total) : '0' ?> result(s) returned</p>
 </div>
 
 <?php echo HTMLstuff::Button('index.php'); ?>
