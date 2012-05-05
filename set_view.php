3a6fd4867473aba8a3e416c04c420799b0af4f0f
fwpost <fwpost@gmail.com>
2012-05-05 18:56:11 +0000

    set_view translated

--- set_view.php
+++ set_view.php
@@ -43,7 +43,7 @@ if($SetID != null)
 }
 else
 {
-	$Set = new Set(null, 'New');
+	$Set = new Set(null, $lang->g('New'));
 	$Model = Model::GetModels(sprintf('model_id = %1$d AND mut_deleted = -1', $ModelID));
 	
 	if($Model) { $Model = $Model[0]; }
@@ -152,7 +152,13 @@ if(array_key_exists('hidAction', $_POST) && $_POST['hidAction'] == 'SetView')
 	}
 }
 
-echo HTMLstuff::HtmlHeader($Model->GetShortName(true).' - Set', $CurrentUser);
+echo HTMLstuff::HtmlHeader(sprintf('%1$s - %2$s - %3$s',
+		$Model->GetShortName(true),
+		$lang->g('NavigationSets'),
+		$Set->getName()
+	),
+	$CurrentUser
+);
 
 if($SetID)
 {
@@ -169,36 +175,40 @@ if($SetID)
 
 ?>
 
-<h2><?php echo sprintf('<a href="index.php">Home</a> - <a href="model_view.php?model_id=%1$d">%2$s</a> - <a href="set.php?model_id=%1$d">Sets</a> - %3$s',
+<h2><?php echo sprintf(
+	'<a href="index.php">%5$s</a> - <a href="model_view.php?model_id=%1$d">%3$s</a> - <a href="set.php?model_id=%1$d">%6$s</a> -  %7$s %4$s',
 	$ModelID,
+	$SetID,
 	htmlentities($Model->GetShortName(true)),
-	htmlentities($Set->getName())
+	htmlentities($Set->getName()),
+	$lang->g('NavigationHome'),
+	$lang->g('NavigationSets'),
+	$lang->g('NavigationSet')
 ); ?></h2>
 
 <form action="<?php echo htmlentities($_SERVER['REQUEST_URI']);?>" method="post">
 <fieldset>
-<legend>Please fill in these fields:</legend>
 
 <input type="hidden" id="hidAction" name="hidAction" value="SetView" />
 
 <div class="FormRow">
-<label for="txtPrefix">Prefix:</label>
+<label for="txtPrefix"><?php echo $lang->g('LabelPrefix');?>:</label>
 <input type="text" id="txtPrefix" name="txtPrefix" maxlength="100" value="<?php echo $Set->getPrefix();?>"<?php echo HTMLstuff::DisabledStr($DeleteSet); ?> />
 </div>
 
 <div class="FormRow">
-<label for="txtName">Name: <em>*</em></label>
+<label for="txtName"><?php echo $lang->g('LabelName');?>: <em>*</em></label>
 <input type="text" id="txtName" name="txtName" maxlength="100" value="<?php echo $Set->getName();?>"<?php echo HTMLstuff::DisabledStr($DeleteSet); ?> />
 </div>
 
 <div class="FormRow">
-<label>Contains: </label>
+<label><?php echo $lang->g('LabelContains');?>: </label>
 <input type="radio" id="radImages" name="radContains" value="<?php echo SET_CONTENT_IMAGE; ?>"<?php echo ($Set->getContainsWhat() & SET_CONTENT_IMAGE) > 0 ? ' checked="checked"' : null; ?><?php echo HTMLstuff::DisabledStr($DeleteSet); ?> /> 
-<label for="radImages" class="Radio">Images</label>
+<label for="radImages" class="Radio"><?php echo $lang->g('NavigationImages');?></label>
 <input type="radio" id="radVideos" name="radContains" value="<?php echo SET_CONTENT_VIDEO; ?>"<?php echo ($Set->getContainsWhat() & SET_CONTENT_VIDEO) > 0 ? ' checked="checked"' : null; ?><?php echo HTMLstuff::DisabledStr($DeleteSet); ?> /> 
-<label for="radVideos" class="Radio">Videos</label>
+<label for="radVideos" class="Radio"><?php echo $lang->g('NavigationVideos');?></label>
 <input type="radio" id="radBoth" name="radContains" value="<?php echo (SET_CONTENT_IMAGE + SET_CONTENT_VIDEO); ?>"<?php echo (($Set->getContainsWhat() & SET_CONTENT_IMAGE) > 0 && ($Set->getContainsWhat() & SET_CONTENT_VIDEO) > 0) ? ' checked="checked"' : null; ?><?php echo HTMLstuff::DisabledStr($DeleteSet); ?> /> 
-<label for="radBoth" class="Radio">Both</label>
+<label for="radBoth" class="Radio"><?php echo $lang->g('LabelBoth');?></label>
 </div>
 
 <?php
@@ -238,28 +248,28 @@ foreach ($DatesThisSet as $Date)
 ?>
 
 <div class="FormRow">
-<label for="txtTags">Tags (CSV):</label>
+<label for="txtTags"><?php echo $lang->g('LabelTags');?> (CSV):</label>
 <input type="text" id="txtTags" name="txtTags" maxlength="200" class="TagsBox" value="<?php echo Tag2All::Tags2AllCSV($TagsThisSet); ?>"<?php echo HTMLstuff::DisabledStr($DeleteSet); ?> />
 </div>
 
 <div class="FormRow">
 <label>&nbsp;</label>
-<input type="submit" class="FormButton" value="<?php echo $DeleteSet ? 'Delete' : 'Save'; ?>" />
-<input type="button" class="FormButton" value="Cancel" onclick="window.location='<?php echo $ReturnURL; ?>';" />
-<input type="button" class="FormButton" value="Clear cacheimage(s)" onclick="window.location='cacheimage_delete.php?set_id=<?php echo $SetID ?>';"<?php echo HTMLstuff::DisabledStr($DeleteSet); ?> />
+<input type="submit" class="FormButton" value="<?php echo $DeleteSet ? $lang->g('ButtonDelete') : $lang->g('ButtonSave'); ?>" />
+<input type="button" class="FormButton" value="<?php echo $lang->g('ButtonCancel');?>" onclick="window.location='<?php echo $ReturnURL; ?>';" />
+<input type="button" class="FormButton" value="<?php echo $lang->g('ButtonClearCacheImage');?>" onclick="window.location='cacheimage_delete.php?set_id=<?php echo $SetID ?>';"<?php echo HTMLstuff::DisabledStr($DeleteSet); ?> />
 </div>
 
 <div class="Separator"></div>
 
 <?php
 	if($Set && ($Set->getContainsWhat() & SET_CONTENT_IMAGE) > 0) 
-	{ echo HTMLstuff::Button(sprintf('image.php?model_id=%1$d&amp;set_id=%2$d', $ModelID, $SetID), 'Images'); }
+	{ echo HTMLstuff::Button(sprintf('image.php?model_id=%1$d&amp;set_id=%2$d', $ModelID, $SetID), $lang->g('NavigationImages')); }
 	
 	if($Set && ($Set->getContainsWhat() & SET_CONTENT_VIDEO) > 0) 
-	{ echo HTMLstuff::Button(sprintf('video.php?model_id=%1$d&amp;set_id=%2$d', $ModelID, $SetID), 'Videos'); }
+	{ echo HTMLstuff::Button(sprintf('video.php?model_id=%1$d&amp;set_id=%2$d', $ModelID, $SetID), $lang->g('NavigationVideos')); }
 ?>
 
-<?php echo HTMLstuff::Button(sprintf('set.php?model_id=%1$d', $ModelID), 'Sets'); ?>
+<?php echo HTMLstuff::Button(sprintf('set.php?model_id=%1$d', $ModelID), $lang->g('NavigationSets')); ?>
 
 <?php echo HTMLstuff::Button('index.php'); ?>
 
