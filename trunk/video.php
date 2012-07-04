fe5c1aac8a019eedaf31185108a567e12fdfc048
fwpost@gmail.com <fwpost@gmail.com@10f31350-ffcc-fd3b-3c78-7cefcf398c7b>
2012-07-04 10:08:07 +0000

    Translated video.php

--- trunk/video.php
+++ trunk/video.php
@@ -44,9 +44,9 @@ if($Videos)
         	"<td class=\"Center\">%5\$s</td>".
 	    	"<td class=\"Center\">%6\$s</td>".
         	"<td>%7\$s</td>".
-			"<td class=\"Center\"><a href=\"download_vid.php?video_id=%1\$d\"><img src=\"images/button_download.png\" width=\"16\" height=\"16\" alt=\"Download video\" title=\"Download video\" /></a></td>".
+			"<td class=\"Center\"><a href=\"download_vid.php?video_id=%1\$d\"><img src=\"images/button_download.png\" width=\"16\" height=\"16\" alt=\"%11\$s\" title=\"%11\$s\" /></a></td>".
 			"<td class=\"Center\"><a href=\"download_image.php?video_id=%1\$d&amp;width=800&amp;height=600\" rel=\"lightbox-thumb\" title=\"Thumbnails of %4\$s\"><img src=\"images/button_view.png\" width=\"16\" height=\"16\" alt=\"Thumbnails of %4\$s\" /></a></td>".
-			"<td class=\"Center\"><a href=\"video_view.php?model_id=%3\$d&amp;set_id=%2\$d&amp;video_id=%1\$d&amp;cmd=%9\$s\" title=\"Delete video\"><img src=\"images/button_delete.png\" width=\"16\" height=\"16\" alt=\"Delete\" /></a></td>".
+			"<td class=\"Center\"><a href=\"video_view.php?model_id=%3\$d&amp;set_id=%2\$d&amp;video_id=%1\$d&amp;cmd=%9\$s\" title=\"%13\$s\"><img src=\"images/button_delete.png\" width=\"16\" height=\"16\" alt=\"%12\$s\" /></a></td>".
         "</tr>",
 		$Video->getID(),
 		$Set->getID(),
@@ -57,7 +57,10 @@ if($Videos)
 		htmlentities($Video->getFileCheckSum()),
 		null,
 		COMMAND_DELETE,
-		$VideoCount % 2 == 0 ? 2 : 1
+		$VideoCount % 2 == 0 ? 2 : 1,
+		$lang->g('LabelDownloadVideo'),
+		$lang->g('ButtonDelete'),
+		$lang->g('LabelDeleteVideo')
 		);
 	}
 }
@@ -77,16 +80,24 @@ if(!$Set)
 	}
 }
 
-echo HTMLstuff::HtmlHeader(sprintf('%1$s - Set %2$s - Videos', $Model->GetShortName(true), $Set->getName()), $CurrentUser);
-
+echo HTMLstuff::HtmlHeader(sprintf('%1$s - %3$s %2$s - %4$s',
+	$Model->GetShortName(true),
+	$Set->getName(),
+	$lang->g('NavigationSet'),
+	$lang->g('NavigationVideos')
+), $CurrentUser);
 ?>
 
 <h2><?php echo sprintf(
-	'<a href="index.php">Home</a> - <a href="model_view.php?model_id=%1$d">%3$s</a> - <a href="set.php?model_id=%1$d">Sets</a> - <a href="set_view.php?model_id=%1$d&amp;set_id=%2$d">Set %4$s</a> - Videos',
+	'<a href="index.php">%5$s</a> - <a href="model_view.php?model_id=%1$d">%3$s</a> - <a href="set.php?model_id=%1$d">%6$s</a> - <a href="set_view.php?model_id=%1$d&amp;set_id=%2$d">%7$s %4$s</a> - %8$s',
 	$ModelID,
 	$SetID,
 	htmlentities($Model->GetShortName(true)),
-	htmlentities($Set->getName())
+	htmlentities($Set->getName()),
+	$lang->g('NavigationHome'),
+	$lang->g('NavigationSets'),
+	$lang->g('NavigationSet'),
+	$lang->g('NavigationVideos')
 ); ?></h2>
 
 <div class="Separator"></div>
@@ -94,10 +105,10 @@ echo HTMLstuff::HtmlHeader(sprintf('%1$s - Set %2$s - Videos', $Model->GetShortN
 <table border="0" cellpadding="4" cellspacing="0">
 	<thead>
 		<tr>
-			<th>Filename</th>
-			<th style="width: 60px;">Extension</th>
-			<th style="width: 80px;">Filesize</th>
-			<th style="width: 290px;">Checksum</th>
+			<th><?php echo $lang->g('LabelFilename');?></th>
+			<th style="width: 60px;"><?php echo $lang->g('LabelExtension');?></th>
+			<th style="width: 80px;"><?php echo $lang->g('LabelFilesize');?></th>
+			<th style="width: 290px;"><?php echo $lang->g('LabelChecksum');?></th>
 			<th style="width: 22px;">&nbsp;</th>
 			<th style="width: 22px;">&nbsp;</th>
 			<th style="width: 22px;">&nbsp;</th>
@@ -105,7 +116,7 @@ echo HTMLstuff::HtmlHeader(sprintf('%1$s - Set %2$s - Videos', $Model->GetShortN
 	</thead>
 	<tfoot>
 		<tr>
-			<th colspan="7">Total Video count: <?php printf('%1$d', $VideoCount); ?></th>
+			<th colspan="7"><?php printf('%1$s: %2$d', $lang->g('LabelTotalVideoCount'), $VideoCount); ?></th>
 		</tr>
 	</tfoot>
 	<tbody>
@@ -115,13 +126,13 @@ echo HTMLstuff::HtmlHeader(sprintf('%1$s - Set %2$s - Videos', $Model->GetShortN
 
 <?php
 
-echo HTMLstuff::Button(sprintf('video_view.php?model_id=%1$d&amp;set_id=%2$d', $ModelID, $SetID), 'New video');
+echo HTMLstuff::Button(sprintf('video_view.php?model_id=%1$d&amp;set_id=%2$d', $ModelID, $SetID), $lang->g('LabelNewVideo'));
 
-echo HTMLstuff::Button(sprintf('import_video.php?model_id=%1$d&amp;set_id=%2$d', $ModelID, $SetID), 'Import videos');
+echo HTMLstuff::Button(sprintf('import_video.php?model_id=%1$d&amp;set_id=%2$d', $ModelID, $SetID), $lang->g('ButtonImportVideos'));
 
-echo HTMLstuff::Button(sprintf('set.php?model_id=%1$d', $ModelID), 'Sets');
+echo HTMLstuff::Button(sprintf('set.php?model_id=%1$d', $ModelID), $lang->g('NavigationSets'));
 
-echo HTMLstuff::Button('index.php');
+echo HTMLstuff::Button();
 
 echo HTMLstuff::HtmlFooter($CurrentUser);
 ?>
